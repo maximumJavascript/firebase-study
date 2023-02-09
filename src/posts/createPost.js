40856e33aa3b22278a154911d39419d85cff1387
Polo <artem-gribanof@yandex.ru>
2023-02-09 19:49:09 +0300

    основная логика создания постов + fix у стилей Views

--- src/posts/createPost.js
+++ src/posts/createPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { authService } from "../auth/auth.service";
+import { createPostService } from "./createPost.service";
 
 export class CreatePost extends Component {
 	constructor(props) {
@@ -7,7 +8,28 @@ export class CreatePost extends Component {
 		if (!authService.isAuth) window.location.pathname = "/login";
 	}
 
+	handleFormSubmit = (e) => {
+		e.preventDefault();
+		const target = e.target;
+		createPostService
+			.handleCreatePost({
+				title: target.title.value,
+				text: target.text.value,
+			})
+			.then(console.log);
+	};
+
 	render() {
-		return <div className="createPostPage">hello world</div>;
+		return (
+			<div className="createPostPage">
+				<form onSubmit={this.handleFormSubmit}>
+					<input name="title" type="text" id="postTitle" placeholder="Заголовок" />
+					<br />
+					<textarea name="text" id="postText" cols="30" rows="10"></textarea>
+					<br />
+					<button type="submit">Отправить</button>
+				</form>
+			</div>
+		);
 	}
 }
