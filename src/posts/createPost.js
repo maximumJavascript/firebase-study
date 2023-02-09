efae585b3f69cbc4294cdca1a2afa2d9e71b1b5e
Polo <artem-gribanof@yandex.ru>
2023-02-09 20:18:45 +0300

    обернул компонент в observer

--- src/posts/createPost.js
+++ src/posts/createPost.js
@@ -1,34 +1,37 @@
 import React, { Component } from "react";
+import { observer } from "mobx-react";
 import { authService } from "../auth/auth.service";
 import { createPostService } from "./createPost.service";
 
-export class CreatePost extends Component {
-	constructor(props) {
-		super(props);
-		if (!authService.isAuth) window.location.pathname = "/login";
-	}
+export const CreatePost = observer(
+	class CreatePost extends Component {
+		constructor(props) {
+			super(props);
+			if (!authService.isAuth) window.location.pathname = "/login";
+		}
 
-	handleFormSubmit = async (e) => {
-		e.preventDefault();
-		await createPostService.handleCreatePost({
-			title: e.target.title.value,
-			text: e.target.text.value,
-		});
-		e.target.title.value = "";
-		e.target.text.value = "";
-	};
+		handleFormSubmit = async (e) => {
+			e.preventDefault();
+			await createPostService.handleCreatePost({
+				title: e.target.title.value,
+				text: e.target.text.value,
+			});
+			e.target.title.value = "";
+			e.target.text.value = "";
+		};
 
-	render() {
-		return (
-			<div className="createPostPage">
-				<form onSubmit={this.handleFormSubmit}>
-					<input name="title" type="text" id="postTitle" placeholder="Заголовок" />
-					<br />
-					<textarea name="text" id="postText" cols="30" rows="10"></textarea>
-					<br />
-					<button type="submit">Отправить</button>
-				</form>
-			</div>
-		);
+		render() {
+			return (
+				<div className="createPostPage">
+					<form onSubmit={this.handleFormSubmit}>
+						<input name="title" type="text" id="postTitle" placeholder="Заголовок" />
+						<br />
+						<textarea name="text" id="postText" cols="30" rows="10"></textarea>
+						<br />
+						<button type="submit">Отправить</button>
+					</form>
+				</div>
+			);
+		}
 	}
-}
+);
