f4a9de45322b8bda6e9f87868678f27c0e5b020b
Alexander <alexanderlysenko99@mail.ru>
2023-02-22 15:32:55 +0300

    формат

--- src/home/PostItem/Author/authorService.js
+++ src/home/PostItem/Author/authorService.js
@@ -1,17 +1,15 @@
-import { toJS } from "mobx";
-import React from "react";
-import { userService } from "../../../usersService/UserService";
-
+import { toJS } from 'mobx';
+import React from 'react';
+import { userService } from '../../../usersService/UserService';
 
 class AuthorService extends React.Component {
-    componentDidMount() {
+  componentDidMount() {
     void userService.getUsers();
-    }
-    getUser = (uid) => {
-      const userList = userService.data;
-    
-      return userList.find((user) =>  user.user.userUid === uid ? user : undefined);
-    }
+  }
+  getUser = (uid) => {
+    const userList = userService.data;
+    return userList.find((user) => (user.user.userUid === uid ? user : undefined));
+  };
 }
 
-export const authorService = new AuthorService()
+export const authorService = new AuthorService();
