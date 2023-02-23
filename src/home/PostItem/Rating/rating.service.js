32e150c8fc4279da246f43d4e98b0749bb8e92f8
Polo <artem-gribanof@yandex.ru>
2023-02-24 02:28:58 +0300

    изменение добавленного рейтинга

--- src/home/PostItem/Rating/rating.service.js
+++ src/home/PostItem/Rating/rating.service.js
@@ -1,5 +1,13 @@
-import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
-import { makeObservable, observable, runInAction } from 'mobx';
+import {
+  addDoc,
+  collection,
+  doc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+import { makeObservable, observable } from 'mobx';
 import { db, auth } from '../../../firebase-config';
 
 class RatingService {
@@ -13,13 +21,13 @@ class RatingService {
   }
 
   getAverageScore = async (postId) => {
-    const ratings = await this.findRatings(postId);
+    const ratings = await this.getRatings(postId);
     const sum = ratings.reduce((acc, obj) => acc + obj.score, 0);
     if (!ratings.length) return 0;
     return Math.round(sum / ratings.length);
   };
 
-  findRatings = async (postId) => {
+  getRatings = async (postId) => {
     const arr = [];
     const q = query(this._collection, where('postId', '==', postId));
     const querySnapshot = await getDocs(q);
@@ -29,12 +37,43 @@ class RatingService {
     return arr;
   };
 
+  getSingleRating = async (postId, userId) => {
+    let obj;
+    const q = query(
+      this._collection,
+      where('postId', '==', postId),
+      where('userId', '==', userId)
+    );
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      obj = {
+        id: doc.id,
+        ...doc.data(),
+      };
+    });
+    return obj;
+  };
+
+  changeRating = async (docId, newScore) => {
+    const ref = doc(db, 'ratings', docId);
+    await updateDoc(ref, { score: newScore });
+  };
+
   addRating = async (postId, score) => {
+    const userId = auth.currentUser.uid;
+    const userRating = await this.getSingleRating(postId, userId);
+    if (userRating) {
+      await this.changeRating(userRating.id, score);
+      // изменили
+      return false;
+    }
     await addDoc(this._collection, {
       postId,
       score,
-      userId: auth.currentUser.uid,
+      userId,
     });
+    // успешно добавили
+    return true;
   };
 }
 
