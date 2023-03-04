ec641fa9cfb76de177ff683cd24d11e12efc33d2
Igorsml <igorsmlff@gmail.com>
2023-03-04 22:43:35 +0300

    fix: import app js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { signOut } from 'firebase/auth';
+import { auth } from './firebase-config';
 import CreatePost from './posts';
 import Home from './home';
 import Login from './auth';
 import NavBar from './header/MainNavbar/NavBar';
-import {Button} from './sandbox/button';
-import {AnimalsDropdown} from './sandbox/dropdown/AnimalsDropdown';
+import { Button } from './sandbox/button/';
+import { Dropdown } from './sandbox/dropdown/';
+import { authService } from './auth/auth.service';
 import Comments from './comments/PostComments';
 
-function Huita() {
-  return (
-    <div>
-      <Button label={'Text button'} />
-      <div style={{margin: '30px'}}>
-        <AnimalsDropdown />
-        test text
-      </div>
-    </div>
-  )
-}
-
 function App() {
-  // const signUserOut = () => {
-  //   signOut(auth).then(() => {
-  //     authService.setSrc(null);
-  //     authService.handleIsAuth();
-  //     window.location.pathname = '/login';
-  //   });
-  // };
+  const signUserOut = () => {
+    signOut(auth).then(() => {
+      authService.setSrc(null);
+      authService.handleIsAuth();
+      window.location.pathname = '/login';
+    });
+  };
+
+  const optionsList = [
+    { id: 1, value: 'Dog', label: 'Dog' },
+    { id: 2, value: 'Cat', label: 'Cat' },
+    { id: 3, value: 'Frontender', label: 'Frontender' },
+  ];
 
   return (
     <Router>
@@ -37,8 +34,9 @@ function App() {
         <Route path="/createpost" element={<CreatePost />} />
         <Route path="/login" element={<Login />} />
         <Route path="/comments/:id" element={<Comments />} />
-        <Route path="/sandbox" element={<Huita />} />
       </Routes>
+      <Button label={'Text button'} />
+      <Dropdown optionsList={optionsList} />
     </Router>
   );
 }
