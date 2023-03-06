3349eb66fe7f4152c028a5d2f5169dea5f2d4c20
Igorsml <igorsmlff@gmail.com>
2023-03-06 17:44:30 +0300

    feat: click outside ok

--- src/sandbox/dropdown/Dropdown.js
+++ src/sandbox/dropdown/Dropdown.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './Dropdown.module.css';
 import { ReactComponent as SvgArrowDown } from '../../assets/icons/SvgArrowDown.svg';
 import Option from './Option';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 export class Dropdown extends React.Component {
   constructor(props) {
@@ -15,51 +15,54 @@ export class Dropdown extends React.Component {
     this.setState((prevState) => ({
       isOpen: !prevState.isOpen,
       isRotate: !prevState.isRotate,
-
     }));
   };
 
   componentDidMount() {
-    document.addEventListener("mousedown", this.handleClickOutside);
+    document.addEventListener('mousedown', this.handleClickOutside);
   }
 
   componentWillUnmount() {
-    document.removeEventListener("mousedown", this.handleClickOutside);
+    document.removeEventListener('mousedown', this.handleClickOutside);
   }
 
   handleClickOutside = (event) => {
     if (this.wrapperRef && !this.wrapperRef.current.contains(event.target)) {
-      this.setState((prevState) => ({
-        isOpen: !prevState.isOpen,
-        isRotate: !prevState.isRotate,
-      }));
+      this.setState({
+        isOpen: false,
+        isRotate: false,
+      });
     }
-  }
+  };
 
   render() {
     return (
       <div className={styles.dropdownContainer}>
-      <div ref={this.wrapperRef} onClick={this.handleClickOutside}>
-        <div className={styles.dropdownMenu} onClick={this.toggleIsOpen}>
-          <div>{this.props.selectedValue}</div>
-          <SvgArrowDown className={this.state.isRotate ? 
-          styles.dropdownMenuIconRotate : styles.dropdownMenuIcon 
-          }/>
-        </div>
-        {this.state.isOpen && (
-          <div className={styles.dropdownItems}>
-            {this.props.optionsList.map((option) => (
-              <Option
-                key={option.id}
-                optionObj={option}
-                onSelectItem={this.props.onSelectItem}
-                onToggle={this.toggleIsOpen}
-                isSelected={this.props.selectedItemIdState === option.id}
-                selectedItemIdState={this.props.selectedItemIdState}
-              />
-            ))}
+        <div ref={this.wrapperRef}>
+          <div className={styles.dropdownMenu} onClick={this.toggleIsOpen}>
+            <div>{this.props.selectedValue}</div>
+            <SvgArrowDown
+              className={
+                this.state.isRotate
+                  ? styles.dropdownMenuIconRotate
+                  : styles.dropdownMenuIcon
+              }
+            />
           </div>
-        )}
+          {this.state.isOpen && (
+            <div className={styles.dropdownItems}>
+              {this.props.optionsList.map((option) => (
+                <Option
+                  key={option.id}
+                  optionObj={option}
+                  onSelectItem={this.props.onSelectItem}
+                  onToggle={this.toggleIsOpen}
+                  isSelected={this.props.selectedItemIdState === option.id}
+                  selectedItemIdState={this.props.selectedItemIdState}
+                />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     );
