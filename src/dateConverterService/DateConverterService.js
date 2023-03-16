a72b943b037d51c5893b10d8c70284542cbd8591
Alexander <alexanderlysenko99@mail.ru>
2023-03-16 16:32:28 +0300

    слегка укоротил

--- src/dateConverterService/DateConverterService.js
+++ src/dateConverterService/DateConverterService.js
@@ -1,29 +1,8 @@
 export class DateConverterService {
   static convertDate = (UNIX_timestamp) => {
-    const a = new Date(UNIX_timestamp * 1000);
-    const months = [
-      'Jan',
-      'Feb',
-      'Mar',
-      'Apr',
-      'May',
-      'Jun',
-      'Jul',
-      'Aug',
-      'Sep',
-      'Oct',
-      'Nov',
-      'Dec',
-    ];
-    const year = a.getFullYear();
-    const month = months[a.getMonth()];
-    const date = a.getDate();
-    const hour = a.getHours();
-    let min = a.getMinutes();
-    min = min < 10 ? '0' + min : min;
-    let sec = a.getSeconds();
-    sec = sec < 10 ? '0' + sec : sec;
-    const time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
-    return time;
+    return new Intl.DateTimeFormat('ru-RU', {
+      dateStyle: 'long',
+      timeStyle: 'medium',
+    }).format(new Date(UNIX_timestamp * 1000));
   };
 }
