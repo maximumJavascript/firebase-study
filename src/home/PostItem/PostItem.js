bed5abbf3fb063bb5fc7704db0f538b447c1ec85
Polo <artem-gribanof@yandex.ru>
2023-05-12 23:20:43 +0300

    установка пакета spring

--- src/home/PostItem/PostItem.js
+++ src/home/PostItem/PostItem.js
@@ -8,6 +8,7 @@ import React from 'react';
 import { auth } from '../../firebase-config';
 import { onAuthStateChanged } from 'firebase/auth';
 import CommentLoginButton from '../../controls/CommentLoginButton/CommentLoginButton';
+import { animated, Spring } from '@react-spring/web';
 
 class PostItem extends React.Component {
   ref = React.createRef();
@@ -28,38 +29,47 @@ class PostItem extends React.Component {
     const src = props.post.base64Img;
     const postUserUid = props.post.author.id;
     return (
-      <div className={styles.post} data-postid={props.post.id} ref={this.ref}>
-        {src && (
-          <div className={styles.postImage}>
-            <img src={src} alt="post: img" />
-          </div>
-        )}
-        <div className={styles.postContainer}>
-          <div className={styles.postBodyText}>
-            <div className={styles.postTitle}>{props.post.title}</div>
-            <div className={styles.postTextContainer}>{props.post.text}</div>
-          </div>
-          <div className={styles.postFooter}>
-            <Author date={props.date} authorId={props.post.author.id} />
-            <Views postId={props.post.id} viewCounter={props.viewCounter} />
-            <Rating postId={props.post.id} />
-            {!props.isComments && (
-              <Link to={`/comments/${props.post.id}`}>
-                <div className={styles.postShowMore}>
-                  <SvgNext />
-                </div>
-              </Link>
+      <Spring>
+        {(s) => (
+          <animated.div
+            className={styles.post}
+            data-postid={props.post.id}
+            ref={this.ref}
+            style={s}
+          >
+            {src && (
+              <div className={styles.postImage}>
+                <img src={src} alt="post: img" />
+              </div>
             )}
+            <div className={styles.postContainer}>
+              <div className={styles.postBodyText}>
+                <div className={styles.postTitle}>{props.post.title}</div>
+                <div className={styles.postTextContainer}>{props.post.text}</div>
+              </div>
+              <div className={styles.postFooter}>
+                <Author date={props.date} authorId={props.post.author.id} />
+                <Views postId={props.post.id} viewCounter={props.viewCounter} />
+                <Rating postId={props.post.id} />
+                {!props.isComments && (
+                  <Link to={`/comments/${props.post.id}`}>
+                    <div className={styles.postShowMore}>
+                      <SvgNext />
+                    </div>
+                  </Link>
+                )}
 
-            {this.state.currentUsserUid === postUserUid ? (
-              <CommentLoginButton
-                onClick={() => this.props.deletePostItem(props.post.id)}
-                text={'DEL'}
-              />
-            ) : null}
-          </div>
-        </div>
-      </div>
+                {this.state.currentUsserUid === postUserUid ? (
+                  <CommentLoginButton
+                    onClick={() => this.props.deletePostItem(props.post.id)}
+                    text={'DEL'}
+                  />
+                ) : null}
+              </div>
+            </div>
+          </animated.div>
+        )}
+      </Spring>
     );
   }
 }
