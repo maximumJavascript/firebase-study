d189e720d513b2a9040382af938b3c0ff0f6ddc6
Polo <artem-gribanof@yandex.ru>
2023-05-14 22:36:50 +0300

    комменты справа от поста

--- src/comments/PostComments/PostComments.js
+++ src/comments/PostComments/PostComments.js
@@ -3,29 +3,17 @@ import { Component } from 'react';
 import { commentsService } from './postComments.service';
 import { matchPath } from 'react-router-dom';
 import PostItem from '../../home/PostItem';
-import homeStyles from '../../home/home.module.css';
-import styles from './PostComments.module.css';
+import navStyles from '../../header/Navbar.module.css';
 import CommentsList from '../CommentsList';
 import CreateComment from '../CreateComment';
-import { toJS } from 'mobx';
+import styles from './PostComments.module.css';
+import homeStyles from '../../home/home.module.css';
 import classNames from 'classnames';
 
 const PostComments = observer(
   class PostComments extends Component {
     constructor(props) {
       super(props);
-      // const match = matchPath(
-      //   {
-      //     path: '/comments/:id',
-      //   },
-      //   window.location.pathname
-      // );
-      // console.log(
-      //   'window.location.pathname',
-      //   window.location.hash.split('/').at(-1)
-      // );
-      // console.log('match', match);
-      // this.id = match?.params.id;
       this.id = window.location.hash.split('/').at(-1);
     }
 
@@ -36,11 +24,7 @@ const PostComments = observer(
     render() {
       const post = commentsService.post;
       if (post.id !== this.id) return null;
-      const postStyles = classNames(
-        homeStyles.container,
-        homeStyles.home,
-        styles.commentsWrap
-      );
+      const postStyles = classNames(navStyles.navbarContainer, styles.commentsWrap);
       return (
         <div className={postStyles}>
           <div className={`${homeStyles.homePage} ${styles.commentsPost}`}>
@@ -51,11 +35,11 @@ const PostComments = observer(
               date={post.date.seconds}
             />
           </div>
-          <div>
+          <div className={styles.commentsActionWrap}>
             <CreateComment postId={post.id} />
-          </div>
-          <div className={styles.commentsList}>
-            <CommentsList postId={post.id} />
+            <div className={styles.commentsList}>
+              <CommentsList postId={post.id} />
+            </div>
           </div>
         </div>
       );
--- src/header/MainNavbar/NavBar.js
+++ src/header/MainNavbar/NavBar.js
@@ -2,12 +2,15 @@ import React from 'react';
 import LeftNavbar from '../LeftPartOfNavbar/LeftNavbarPart';
 import CentrNavbar from '../CentrPartOfNavbar/CentrNavbar';
 import RightNavbar from '../RightPartOfNavbar/RightNavbar';
+import styles from '../Navbar.module.css';
+import classNames from 'classnames';
 
 class NavBar extends React.Component {
   render() {
+    const navClasses = classNames(styles.navbarContainer, styles.nav);
     return (
       <header>
-        <nav>
+        <nav className={navClasses}>
           <LeftNavbar />
           <CentrNavbar signUserOut={this.props.signUserOut} />
           <RightNavbar photoSrc={this.props.photoSrc} />
