74c0a10bd1563281b97ab141681bcf1fb0d14a62
Polo <artem-gribanof@yandex.ru>
2023-07-06 19:42:30 +0300

    useMemo in notify

--- src/notifications/NotifyItem/NotifyItem.js
+++ src/notifications/NotifyItem/NotifyItem.js
@@ -3,7 +3,7 @@ import cn from 'classnames';
 import { notifyListService } from '../NotifyList/notifyList.service';
 import { NotifyIcon } from '../NotifyIcon';
 import { animated, useSpring, config } from '@react-spring/web';
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { REMOVE_NOTIFY_TIME } from '../../constants/notify';
 
 export function NotifyItem({
@@ -11,21 +11,19 @@ export function NotifyItem({
   autoClose = true,
   timeoutMS = REMOVE_NOTIFY_TIME,
 }) {
-  const [isForceUpdate, forceUpdate] = useState(false);
+  const [componentStyles, setComponentStyles] = useState(null);
   const [isClose, setIsClose] = useState(false);
   const ref = useRef(null);
-  const componentStyles = useRef(null);
   const closeTimeout = useRef(null);
 
   useLayoutEffect(() => {
     const computedStyle = window.getComputedStyle(ref.current);
 
-    componentStyles.current = {
+    setComponentStyles({
       height: ref.current.offsetHeight ? ref.current.offsetHeight + 'px' : '0px',
       paddingTop: computedStyle['padding-top'],
       paddingBottom: computedStyle['padding-bottom'],
-    };
-    forceUpdate(true);
+    });
   }, []);
 
   useEffect(() => {
@@ -48,28 +46,33 @@ export function NotifyItem({
   const { text, status } = notify;
   const notifyCn = cn(styles.notify, styles[status]);
 
-  const springConfig = useSpring({
-    from: {
-      height: '0px',
-      paddingTop: '0px',
-      paddingBottom: '0px',
-      marginTop: '0px',
-      opacity: 0,
-    },
-    to: {
-      height: isClose ? '0px' : componentStyles.current?.height || '0px',
-      paddingTop: isClose ? '0px' : componentStyles.current?.paddingTop || '0px',
-      paddingBottom: isClose ? '0px' : componentStyles.current?.paddingBottom || '0px',
-      marginTop: isClose ? '0px' : '10px',
-      opacity: isClose ? 0 : 1,
-    },
-    config: config.stiff,
-    onRest() {
-      if (isClose) handleDeleteNotify();
-    },
-  });
+  const springConfigObj = useMemo(
+    () => ({
+      from: {
+        height: '0px',
+        paddingTop: '0px',
+        paddingBottom: '0px',
+        marginTop: '0px',
+        opacity: 0,
+      },
+      to: {
+        height: isClose ? '0px' : componentStyles?.height || '0px',
+        paddingTop: isClose ? '0px' : componentStyles?.paddingTop || '0px',
+        paddingBottom: isClose ? '0px' : componentStyles?.paddingBottom || '0px',
+        marginTop: isClose ? '0px' : '10px',
+        opacity: isClose ? 0 : 1,
+      },
+      config: config.stiff,
+      onRest() {
+        if (isClose) handleDeleteNotify();
+      },
+    }),
+    [isClose, componentStyles]
+  );
 
-  const style = isForceUpdate ? springConfig : {};
+  const springConfig = useSpring(springConfigObj);
+
+  const style = componentStyles ? springConfig : {};
   return (
     <animated.div style={style} className={notifyCn} onClick={handleClose} ref={ref}>
       <div className={styles.iconWrap}>
